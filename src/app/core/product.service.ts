5204dd74c75a8322231a58c4fc6b7eb38720f760
Nizar MAATOUG <nizar.maatoug@hotmail.com>
2026-01-15 16:44:55 +0100

    add getAll to product service

--- src/app/core/product.service.ts
+++ src/app/core/product.service.ts
@@ -1,13 +1,32 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { Product } from '../product/product.model';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
 
+  private readonly baseUrl = 'api/products';
+  private readonly json = new HttpHeaders({ 'Content-Type': 'application/json' });
+
   private http = inject(HttpClient);
 
-  
+  readonly products = signal<Product[]>([]);
+
+
+  getAll(): Observable<Product[]> {
+
+    return this.http.get<Product[]>(this.baseUrl).pipe(
+      tap(list => this.products.set(list)),
+    );
+
+  }
+
+
+
+
+
 
 }
