e1977e0916fce7387017f9461a50b4783bf8e0fa
Nizar MAATOUG <nizar.maatoug@hotmail.com>
2026-01-23 09:51:48 +0100

    complete product service

--- src/app/core/product.service.ts
+++ src/app/core/product.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
+import { inject, Injectable, Signal, signal } from '@angular/core';
 import { Product } from '../product/product.model';
-import { Observable, tap } from 'rxjs';
+import { catchError, Observable, tap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -13,15 +13,63 @@ export class ProductService {
 
   private http = inject(HttpClient);
 
+  //gérer l'état des produits: signal initialement vide
   readonly products = signal<Product[]>([]);
+
+  //gérer l'état du produit séléctionné
+  readonly product = signal<Product|null>(null);
+
+  //gérer les erreurs d'API
+  readonly error = signal<String|null>(null);
+
+  //gérer l'état de l'API
+  readonly isLoading = signal<boolean>(false);
 
 
   getAll(): Observable<Product[]> {
-
+    this.isLoading.set(true);
     return this.http.get<Product[]>(this.baseUrl).pipe(
       tap(list => this.products.set(list)),
+      tap(list => this.isLoading.set(false)),
     );
+  }
 
+  getById(id:number): Observable<Product|null>{
+    this.isLoading.set(true);
+    return this.http.get<Product|null>(`{this.baseUrl}/{id}`).pipe(
+      tap(p => this.product.set(p)),
+      tap(p => this.isLoading.set(false)),
+    );
+  }
+
+  create(product: Product){
+    this.isLoading.set(true);
+    return this.http.post<Product>(this.baseUrl,product,{ headers: this.json }).pipe(
+      tap(created => this.getAll()),
+      catchError(err => this.fail(err)),
+    );
+  }
+
+  update(product: Product){
+    this.isLoading.set(true);
+    return this.http.put<Product>(this.baseUrl,product,{ headers: this.json })
+    .pipe(
+      tap(updated => this.getAll()),
+      catchError(err => this.fail(err)),
+    );
+  }
+
+  delete(id: number){
+    this.isLoading.set(true);
+    return this.http.delete(`{this.baseUrl}/{id}`).pipe(
+      tap(this.getAll()),
+      catchError(err => this.fail(err))
+    );
+  }
+
+  fail(err:unknown){
+    const msg = (err as any)?.message ?? 'Erreur inconnue';
+    return throwError(() => new Error(msg));
   }
 
 
